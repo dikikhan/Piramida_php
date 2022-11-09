85e02289ee04ca271070161f21a9ccbf91458b4b
Diki Wahyudy <99858229+dikikhan@users.noreply.github.com>
2022-11-09 21:50:16 +0700

    Add files via upload

---
+++ piramid.php
@@ -0,0 +1,28 @@
+<?php
+for ($i=0;$i<8;$i++){
+	for($z=10; $z>=$i; $z--){
+	echo "&nbsp&nbsp";
+}
+	for($x=0; $x<=$i; $x++){
+	echo "*&nbsp&nbsp";
+	}
+	echo "<br>";
+}
+for ($i=0;$i<5;$i++){
+	for($z=10; $z>=$i; $z--){
+	echo "&nbsp&nbsp";
+}
+	for($x=0; $x<=$i; $x++){
+	echo "*&nbsp&nbsp";
+	}
+	echo "<br>";
+}
+	for ($i=0;$i<=2;$i++){
+	for($z=10; $z>=$i; $z--){
+	echo "&nbsp&nbsp";
+}
+	for($x=0; $x<=$i; $x++){
+	echo "*&nbsp&nbsp";
+	}
+	echo "<br>";
+}
